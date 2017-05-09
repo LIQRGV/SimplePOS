762802e67e96d3472b4cecceb379ae3a757592f4
Septian Hari <septianhari@gmail.com>
2017-05-10 01:06:44 +0700

    Change factory according to new structure

--- spec/factories/employee.rb
+++ spec/factories/employee.rb
@@ -2,28 +2,28 @@ FactoryGirl.define do
   factory :employee do
     username "some_username"
     employee_name "some_name"
-    position "some_position"
     password "some_password"
+    role_id -1
   end
 
   factory :employee_owner do
     username "pemilik_username"
     employee_name "pemilik_name"
-    position "pemilik"
     password "pemilik_password"
+    role_id 1
   end
 
   factory :employee_supervisor do
     username "supervisor_username"
     employee_name "supervisor_name"
-    position "supervisor"
     password "supervisor_password"
+    role_id 2
   end
 
   factory :employee_cashier do
     username "kasir_username"
     employee_name "kasir_name"
-    position "kasir"
     password "kasir_password"
+    role_id 3
   end
 end
