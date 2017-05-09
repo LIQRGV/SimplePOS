91690430c2479d3ad7009526b107b46c61616729
Septian Hari <septianhari@gmail.com>
2017-05-09 22:38:40 +0700

    Add routes for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'auth/login'
+  get 'auth/login' => 'auth#login'
 
-  post 'auth/authorize'
+  post 'auth/authorize' => 'auth#authorize'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
