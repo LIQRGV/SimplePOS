ec1e9b44da12a7f669e90ce79d3e2f83b88a5732
Septian Hari <septianhari@gmail.com>
2017-05-10 00:02:59 +0700

    Add some asset files

--- spec/factories/employee.rb
+++ spec/factories/employee.rb
@@ -5,4 +5,25 @@ FactoryGirl.define do
     position "some_position"
     password "some_password"
   end
+
+  factory :employee_owner do
+    username "pemilik_username"
+    employee_name "pemilik_name"
+    position "pemilik"
+    password "pemilik_password"
+  end
+
+  factory :employee_supervisor do
+    username "supervisor_username"
+    employee_name "supervisor_name"
+    position "supervisor"
+    password "supervisor_password"
+  end
+
+  factory :employee_cashier do
+    username "kasir_username"
+    employee_name "kasir_name"
+    position "kasir"
+    password "kasir_password"
+  end
 end
