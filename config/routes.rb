79f8b8b2421d41195ad0319e8e8bf27be0402c50
Septian Hari <septianhari@gmail.com>
2017-05-19 00:41:26 +0700

    Slightly change the route name of 'change qoute'

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post 'auth/authorize' => 'auth#authorize'
 
   get 'home' => 'home#home'
-  post 'change_quote' => 'home#change_quote'
+  post 'home/change_quote' => 'home#change_quote'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
